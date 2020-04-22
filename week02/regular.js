81063c8b6b78041c6de9d1c915692f2dca1120a2
ZhangYing <1783477613@qq.com>
2020-04-23 01:12:58 +0800

    Update regular.js

--- week02/regular.js
+++ week02/regular.js
@@ -1,7 +1,13 @@
 // 写一个正则表达式 匹配所有 Number 直接量
 function number(val) {
-    // negative or positive
-    return /^[-]?[\.\d]+$/.test(val);
+    // 十进制 0  0. .0
+    // 二进制 0B 0b
+    // 十六进制 0X 0x
+    // 八进制 0o 0O
+    // 12.4E10  12.4e10
+    // - +
+    var par = '/ [0-1]+$ | [0-7]+$ | ([A-F][a-f][0-9])+$ | ^.?[0-9]+(\.?[0-9]+)?$ /g'
+    return par.test(val)
 }
 
 

1b922cb7ffff5d7e0c297138a0d69e9b7d31cb31
ZhangYing <1783477613@qq.com>
2020-04-23 17:42:21 +0800

    Update regular.js

--- week02/regular.js
+++ week02/regular.js
@@ -11,13 +11,6 @@ function number(val) {
 }
 
 
-// 写一个 UTF-8 Encoding 的函数
-function UTF8Encoding(){
-
-}
-
-
-
 // 写一个正则表达式，匹配所有的字符串直接量，单引号和双引号
 function regularString(str){
     var par = '/ ^\"[A-Za-z]+\"$ |  ^\'[A-Za-z]+\'$ /g'

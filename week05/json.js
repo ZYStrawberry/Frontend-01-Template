2e88a67131bbb5137472cd8fa07d96338e76ec63
ZhangYing <1783477613@qq.com>
2020-05-13 17:30:07 +0800

    Create json.js

---
+++ week05/json.js
@@ -0,0 +1,301 @@
+const data = {
+    "nodes": [{
+            "id": "0",
+            "label": "全局对象",
+            "class": "c0"
+        },
+        {
+            "id": "1",
+            "label": "值属性",
+            "class": "c1"
+        },
+        {
+            "id": "2",
+            "label": "Infinity",
+            "class": "c1-1"
+        },
+        {
+            "id": "3",
+            "label": "NaN",
+            "class": "c1-1"
+        },
+        {
+            "id": "4",
+            "label": "undefined",
+            "class": "c1-1"
+        },
+        {
+            "id": "5",
+            "label": "null 字面量",
+            "class": "c1-1"
+        },
+        {
+            "id": "6",
+            "label": "globalThis",
+            "class": "c1-1"
+        },
+        {
+            "id": "7",
+            "label": "函数属性",
+            "class": "c2"
+        },
+
+        {
+            "id": "8",
+            "label": "eval()",
+            "class": "c2-1"
+        },
+        {
+            "id": "9",
+            "label": "uneval()",
+            "class": "c2-1"
+        },
+        {
+            "id": "10",
+            "label": "isFinite()",
+            "class": "c2-1"
+        },
+        {
+            "id": "11",
+            "label": "isNaN()",
+            "class": "c2-1"
+        },
+        {
+            "id": "12",
+            "label": "parseFloat()",
+            "class": "c2-1"
+        },
+        {
+            "id": "13",
+            "label": "parseInt()",
+            "class": "c2-1"
+        },
+        {
+            "id": "14",
+            "label": "decodeURI()",
+            "class": "c2-1"
+        },
+        {
+            "id": "15",
+            "label": "decodeURIComponent()",
+            "class": "c2-1"
+        },
+        {
+            "id": "16",
+            "label": "encodeURI()",
+            "class": "c2-1"
+        },
+        {
+            "id": "17",
+            "label": "encodeURIComponent()",
+            "class": "c2-1"
+        },
+        {
+            "id": "18",
+            "label": "基本对象",
+            "class": "c3"
+        },
+        {
+            "id": "19",
+            "label": "Object",
+            "class": "c3-1"
+        },
+        {
+            "id": "20",
+            "label": "Function",
+            "class": "c3-1"
+        },
+        {
+            "id": "21",
+            "label": "Boolean",
+            "class": "c3-1"
+        },
+        {
+            "id": "22",
+            "label": "Symbol",
+            "class": "c3-1"
+        },
+        {
+            "id": "23",
+            "label": "错误对象",
+            "class": "c4"
+        },
+        {
+            "id": "24",
+            "label": "Error",
+            "class": "c4-1"
+        },
+        {
+            "id": "25",
+            "label": "AggregateError",
+            "class": "c4-1"
+        },
+        {
+            "id": "26",
+            "label": "EvalError",
+            "class": "c4-1"
+        },
+        {
+            "id": "27",
+            "label": "InternalError",
+            "class": "c4-1"
+        },
+        {
+            "id": "28",
+            "label": "RangeError",
+            "class": "c4-1"
+        },
+        {
+            "id": "29",
+            "label": "ReferenceError",
+            "class": "c4-1"
+        },
+        {
+            "id": "30",
+            "label": "SyntaxError",
+            "class": "c4-1"
+        },
+        {
+            "id": "31",
+            "label": "TypeError",
+            "class": "c4-1"
+        },
+        {
+            "id": "32",
+            "label": "URIError",
+            "class": "c4-1"
+        }
+        
+    ],
+    "edges": [{
+            "source": "0",
+            "target": "1"
+        },
+        {
+            "source": "0",
+            "target": "7"
+        },
+        {
+            "source": "0",
+            "target": "18"
+        },
+        {
+            "source": "1",
+            "target": "2"
+        },
+        {
+            "source": "1",
+            "target": "3"
+        },
+        {
+            "source": "1",
+            "target": "4"
+        },
+        {
+            "source": "1",
+            "target": "5"
+        },
+        {
+            "source": "1",
+            "target": "6"
+        },
+        {
+            "source": "7",
+            "target": "8"
+        },
+        {
+            "source": "7",
+            "target": "9"
+        },
+        {
+            "source": "7",
+            "target": "10"
+        },
+        {
+            "source": "7",
+            "target": "11"
+        },
+        {
+            "source": "7",
+            "target": "12"
+        },
+        {
+            "source": "7",
+            "target": "13"
+        },
+        {
+            "source": "7",
+            "target": "14"
+        },
+        {
+            "source": "7",
+            "target": "15"
+        },
+        {
+            "source": "7",
+            "target": "16"
+        },
+        {
+            "source": "7",
+            "target": "17"
+        },
+       
+        {
+            "source": "18",
+            "target": "19"
+        },
+        {
+            "source": "18",
+            "target": "20"
+        },
+        {
+            "source": "18",
+            "target": "21"
+        },
+        {
+            "source": "18",
+            "target": "22"
+        },
+        {
+            "source": "0",
+            "target": "23"
+        },
+        {
+            "source": "23",
+            "target": "24"
+        },
+        {
+            "source": "23",
+            "target": "25"
+        },
+        {
+            "source": "23",
+            "target": "26"
+        },
+        {
+            "source": "23",
+            "target": "27"
+        },
+        {
+            "source": "23",
+            "target": "28"
+        },
+        {
+            "source": "23",
+            "target": "29"
+        },
+        {
+            "source": "23",
+            "target": "30"
+        },
+        {
+            "source": "23",
+            "target": "31"
+        },
+        {
+            "source": "23",
+            "target": "32"
+        },
+        
+    ]
+}

73c0067c17bf3b0ca1f40281dfefe7f478f3f47c
ZYStrawberry <1783477613@qq.com>
2020-08-23 22:26:00 +0800

    update

--- week18/tool-chain/test-demo/test/add.test.js
+++ week18/tool-chain/test-demo/test/add.test.js
@@ -12,8 +12,8 @@ var assert = require('assert');
 
 describe('add', function () {
     it('add(5, 2) should be 7', function () {
-        assert.equal(add.add(5, 2), 7);
-        // assert.equal(add(5, 2), 7);
+        // assert.equal(add.add(5, 2), 7);
+        assert.equal(add(5, 2), 7);
     });
 });
 

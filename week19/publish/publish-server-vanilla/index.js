5af7b756aa0312c299901823877e13282df510ea
zhangying <1783477613@qq.com>
2020-09-04 17:42:30 +0800

    update

--- week19/publish/publish-server-vanilla/index.js
+++ week19/publish/publish-server-vanilla/index.js
@@ -3,8 +3,9 @@ const http = require('http');
 // Create an HTTP server
 const server = http.createServer((req, res) => {
     console.log(res)
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('okay');
+    var writeStream = fs.createWriteStream('../public/')
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('okay');
 });
 
 server.listen(8084)
